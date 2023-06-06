aa44401a89c7373dd69a07f5ca33de5f86e5312d
emohamed <emohamed@shiftev.com>
2023-06-06 21:50:41 +0200

    Add a new endpoint pathes for Delivery and Manager views

--- LittleLemonApi/urls.py
+++ LittleLemonApi/urls.py
@@ -1,6 +1,8 @@
-from LittleLemonApi.views import MenuItemsView, MenuItemsViewSet
+from LittleLemonApi.views import MenuItemsView, MenuItemsViewSet, ManagerView, DeliveryCrewView
 from django.urls import path, include 
 urlpatterns = [
     path('menu-items/', MenuItemsViewSet.as_view({'get': 'list', 'post': 'create', 'put': 'update', 'delete': 'destroy'})),
     path('menu-items/<int:pk>/', MenuItemsView.as_view()),
+    path('groups/manager/users/',ManagerView.as_view() ),
+    path('groups/delivery-crew/users/',DeliveryCrewView.as_view() ),
 ]
