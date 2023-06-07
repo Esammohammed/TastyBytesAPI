4648bbb0551f03ef0b8ed2182a7f2596dd313abf
emohamed <emohamed@shiftev.com>
2023-06-07 15:45:13 +0200

    Add two endpoint for Customer and Order views

--- LittleLemonApi/urls.py
+++ LittleLemonApi/urls.py
@@ -1,8 +1,10 @@
-from LittleLemonApi.views import MenuItemsView, MenuItemsViewSet, ManagerView, DeliveryCrewView
+from LittleLemonApi.views import MenuItemsView, MenuItemsViewSet, ManagerView, DeliveryCrewView,CustomerView, OrderView
 from django.urls import path, include 
 urlpatterns = [
     path('menu-items/', MenuItemsViewSet.as_view({'get': 'list', 'post': 'create', 'put': 'update', 'delete': 'destroy'})),
     path('menu-items/<int:pk>/', MenuItemsView.as_view()),
     path('groups/manager/users/',ManagerView.as_view() ),
     path('groups/delivery-crew/users/',DeliveryCrewView.as_view() ),
+    path('cart/menu-items/', CustomerView.as_view()),
+    path('orders/', OrderView.as_view()),
 ]
