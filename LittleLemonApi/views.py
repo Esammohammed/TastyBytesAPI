3f81dd3a3a1cb6fe50cb1ee9a8ad03c8f8cadf71
emohamed <emohamed@shiftev.com>
2023-06-07 15:46:55 +0200

    Create a new view for customer that handles CRUD requests  for customer

--- LittleLemonApi/views.py
+++ LittleLemonApi/views.py
@@ -1,9 +1,10 @@
+from copy import deepcopy
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from LittleLemonApi.models import MenuItem
+from LittleLemonApi.models import Cart, MenuItem, OrderItem
 from rest_framework import status
 from LittleLemonApi.serializers import MenuItemSerializer
 from LittleLemonApi.permissions import Permissions
@@ -101,3 +102,38 @@ class DeliveryCrewView (APIView):
             deleted_delivery_crew.groups.remove(Group.objects.get(name='Delivery Crew'))
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_403_FORBIDDEN)
+    
+    
+class CustomerView (APIView):
+    
+    def get(self,request):
+        if Permissions.is_customer(request):
+
+            username= request.user
+            items = Cart.objects.filter(user=username)
+            return Response(items.values())
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    
+    def post(self,request):
+        if Permissions.is_customer(request):
+            username= request.user
+            itemname= request.data['title']
+            itemquantity= request.data['quantity']
+            user =User.objects.get(username=username)
+            menuitem = MenuItem.objects.get(title=itemname)
+            unit_price = MenuItem.objects.get(title=itemname).price
+            price = float(unit_price) * float (itemquantity)
+            Cart.objects.create(user=user, menuitem=menuitem, quantity=itemquantity,unit_price=unit_price,price=price)
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    
+    def delete(self,request):
+        if Permissions.is_customer(request):
+            username= request.user
+            user =User.objects.get(username=username)
+            carts = Cart.objects.filter(user=user)
+            carts.delete()
+            
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
