42a158cf87412076aa5f0240634eb63c7bdef7a8
emohamed <emohamed@shiftev.com>
2023-06-07 15:44:39 +0200

    Create a CartSerializer for cart model

--- LittleLemonApi/serializers.py
+++ LittleLemonApi/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from LittleLemonApi.models import MenuItem
+from LittleLemonApi.models import Cart, MenuItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'featured', 'category']
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
