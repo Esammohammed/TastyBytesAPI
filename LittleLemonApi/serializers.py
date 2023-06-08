79f965626a1714f4cc0829fa18a824152fe92cac
Esammohammed <essamonly111@gmail.com>
2023-06-08 17:48:40 +0200

    Create a serializer for order and orderitem models used by order views

--- LittleLemonApi/serializers.py
+++ LittleLemonApi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from LittleLemonApi.models import Cart, MenuItem
+from LittleLemonApi.models import Cart, MenuItem, Order, OrderItem
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,18 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+from rest_framework import serializers
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+                    
