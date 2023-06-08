ce83bdaa401b1d7833fe2bdfccf35837bd738665
Esammohammed <emohamed@shift-ev.com>
2023-06-08 10:14:25 +0200

    Register Cart model to admin page

--- LittleLemonApi/admin.py
+++ LittleLemonApi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from LittleLemonApi.models import MenuItem, Category
+from LittleLemonApi.models import Cart, MenuItem, Category
 # Register your models here.
 admin.site.register(MenuItem)   
 admin.site.register(Category)
+admin.site.register(Cart)
